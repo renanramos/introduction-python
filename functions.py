b75088a3080bd51de38a23aedd89b808622bd237
Renan Rodrigues Ramos (Ci&t) <renanramos@ciandt.com>
2021-11-08 13:36:30 -0300

    Funcoes

---
+++ functions.py
@@ -0,0 +1,13 @@
+def addTwoNumbers(x, y):
+    print(x, y)
+    return x + y
+
+c = addTwoNumbers(2, 5)
+
+print(c)
+
+def printMeu() :
+    print("This is a menu")
+    print("1 - Choice")
+
+printMeu()
